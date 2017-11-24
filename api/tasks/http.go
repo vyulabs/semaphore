e43f0d77d285d4e568c85ce0bae27de1969084cb
Denis <denguk@gmail.com>
2017-11-24 14:49:31 +0500

    feat: panic err

--- api/tasks/http.go
+++ api/tasks/http.go
@@ -14,6 +14,7 @@ import (
 	"strings"
 	"bytes"
 	"errors"
+	"fmt"
 )
 
 func resolveDefaultVersion(versionTemplate string, taskID int, taskNum int) string {
@@ -55,6 +56,7 @@ func ResolveNewVersion(currentVersion string, versionTemplate string, taskID int
 						}
 					}
 					if end == len(currentVersion) {
+						fmt.Println("illegal version format " + currentVersion)
 						return "", errors.New("illegal version format")
 					}
 				} else {
